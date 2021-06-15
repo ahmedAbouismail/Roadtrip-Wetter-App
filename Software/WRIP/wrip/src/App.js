a0c7ea1abfe07dd9d0afcb6ac2c113f044e93467
Leon Kling <beta-code.info@gmx.de>
2021-06-15 15:02:04 +0200

    Frontend Try

--- Software/WRIP/wrip/src/App.js
+++ Software/WRIP/wrip/src/App.js
@@ -2,44 +2,15 @@ import './App.css';
 import {useState} from 'react';
 import RouteEingeben from './components/RouteForm/routeForm'
 import Example from './components/DateForm/dateForm'
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 
-function ListRoute({route}) {
-  return(
-    <div>
-      <h3>{route.country}</h3>
-      <h3>{route.city}</h3>
-    </div>
-  )
-}
 function App() {
-
-  const [forms, setform] = useState(<RouteEingeben/>)
-
- 
-  function addOne(form) {
-    setform([...forms], {country: "", city: ""});
-  }
-  
   return (
-    <div>
-      <RouteEingeben />
-      <Example/>
+    <div className="App">
+      <Navbar />
     </div>
-        
-    // <div className="App">
-    //   <body>
-    //     <nav>
-    //       <h1>Navbar</h1>
-    //     </nav>
-    //     <main>
-    //       <h1>Main</h1>
-    //     </main>
-    //   </body>
-    //   <footer>
-    //       <h1>Footer</h1>
-    //   </footer>
-    // </div>
   );
 }
 
