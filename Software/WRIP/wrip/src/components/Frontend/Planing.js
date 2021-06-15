560e2c8111e17f6333f44e0ebe4c00b81f820c4e
Leon Kling <beta-code.info@gmx.de>
2021-06-15 20:11:35 +0200

    Little Changes

--- Software/WRIP/wrip/src/components/Frontend/Planing.js
+++ Software/WRIP/wrip/src/components/Frontend/Planing.js
@@ -10,7 +10,7 @@ function Cards() {
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='https://images.unsplash.com/photo-1617793910803-dac609647901?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80'
+              src='https://images.unsplash.com/photo-1446776899648-aa78eefe8ed0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fG1hcHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
               text='Here has to be the Inputs!'
               label='BETA'
             />
